03d0ac277a9556a78fd29487203f820c249b4b98
Salvarez99 <stephenalvarez3@gmail.com>
2025-10-17 12:08:14 -0400

    Put in implementation file again

--- Project.App/Project.Api/Repositories/Interface/IRoomPlayerRepository.cs
+++ Project.App/Project.Api/Repositories/Interface/IRoomPlayerRepository.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Project.Api.Models;
-using Project.Api.Enum;
+using Project.Api.Enums;
 
 namespace Project.Api.Repositories.Interface
 {
     public interface IRoomPlayerRepository
     {
-        Task<RoomPlayer?> GetByIdAsync(long id);
+        Task<RoomPlayer?> GetByIdAsync(Guid id);
         Task<IEnumerable<RoomPlayer>> GetAllAsync();
         Task<IEnumerable<RoomPlayer>> GetByRoomIdAsync(Guid roomId);
-        Task<IEnumerable<RoomPlayer>> GetByUserIdAsync(int userId);
-        Task<RoomPlayer?> GetByRoomAndUserAsync(Guid roomId, int userId);
+        Task<IEnumerable<RoomPlayer>> GetByUserIdAsync(Guid userId);
+        Task<RoomPlayer?> GetByRoomAndUserAsync(Guid roomId, Guid userId);
         Task<IEnumerable<RoomPlayer>> GetActivePlayersInRoomAsync(Guid roomId);
         Task<RoomPlayer> CreateAsync(RoomPlayer roomPlayer);
         Task<RoomPlayer> UpdateAsync(RoomPlayer roomPlayer);
-        Task<bool> DeleteAsync(long id);
-        Task<bool> ExistsAsync(long id);
-        Task<bool> IsPlayerInRoomAsync(Guid roomId, int userId);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+        Task<bool> IsPlayerInRoomAsync(Guid roomId, Guid userId);
         Task<int> GetPlayerCountInRoomAsync(Guid roomId);
         Task<RoomPlayer?> GetRoomHostAsync(Guid roomId);
-        Task UpdatePlayerStatusAsync(long id, Status status);
-        Task UpdatePlayerBalanceAsync(long id, long balance);
+        Task UpdatePlayerStatusAsync(Guid id, Status status);
+        Task UpdatePlayerBalanceAsync(Guid id, long balance);
     }
 }
