122fb1548cd99ec61e3e1c1e447d3076f6f11052
Muhiddin Kurbonov <muhiddindc@gmail.com>
2025-10-22 13:30:32 -0500

    Add missing using directive in Program.cs

--- Project.App/Project.Api/Program.cs
+++ Project.App/Project.Api/Program.cs
@@ -13,6 +13,7 @@ using Project.Api.Middleware;
 using Project.Api.Repositories;
 using Project.Api.Repositories.Interface;
 using Project.Api.Services;
+using Project.Api.Services.Interface;
 using Serilog;
 
 namespace Project.Api;
